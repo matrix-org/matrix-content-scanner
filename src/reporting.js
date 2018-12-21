dd707051991c028b21e063daa53dad50a9e4e2c4
Michael Kaye <1917473+michaelkaye@users.noreply.github.com>
2018-12-21 11:04:39 +0000

    Remove duplicated "getConfig" call.
    Both PRs merged for this release introduced a getConfig.

--- src/reporting.js
+++ src/reporting.js
@@ -26,8 +26,6 @@ const decryptFile = require('./decrypt-file.js');
 
 const crypto = require('crypto');
 const { getConfig } = require('./config.js');
-
-const { getConfig } = require('./config.js');
 const fileType = require('file-type');
 
 // Generate a bas64 SHA 256 hash of the input string
