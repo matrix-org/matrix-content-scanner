8499cac0d98622025ddfc442558f4ab5e188191e
Andrew Morgan <1342360+anoadragon453@users.noreply.github.com>
2021-06-23 15:53:21 +0100

    Additionally check file mimetype after decryption (#43)
    Before this change, we would only check the mimetype of an encrypted file based on the information provided with the file by the client.
    
    This change now additionally checks the mimetype of encrypted files based on the file contents (after it has been decrypted) - just as we do for non-encrypted files.

--- src/decrypt-file.js
+++ src/decrypt-file.js
@@ -19,11 +19,9 @@ limitations under the License.
 const decryptData = require('./decrypt.js');
 const fs = require('fs');
 
-// Decrypt a file at `filePath` using decryptData and store the result at `encryptedFilePath`
-module.exports = function decryptFile(filePath, encryptedFilePath, eventContentFile) {
+// Decrypt a file at `filePath` using decryptData and return the decrypted buffer
+module.exports = function decryptFile(filePath, eventContentFile) {
     const data = fs.readFileSync(filePath);
 
-    const buffer = decryptData(data, eventContentFile);
-
-    fs.writeFileSync(encryptedFilePath, buffer);
+    return decryptData(data, eventContentFile);
 }
